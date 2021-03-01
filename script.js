2df2c6febf190b7a6deb7c1f07d4372e0b54a836
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-03-01 17:03:00 +0100

    API(post)

---
+++ script.js
@@ -0,0 +1,38 @@
+(() => {
+const inputs = Array.from(document.querySelectorAll('input'));
+console.log(inputs)
+
+const call = async () => {
+    try {
+        const response = await fetch('https://character-database.becode.xyz/characters');
+        const characters = await response.json();
+         console.log(characters)
+    }
+    catch(err){
+        console.error(err)
+    }
+}
+call()
+// document.querySelector('#add').addEventListener('click',  async () =>{
+//     const values = inputs.map(({value}) => value.trim());
+//     const [name, shortDescription, description, image] = values
+
+//     const response = await fetch('https://character-database.becode.xyz/characters', {
+//         method: 'POST',
+//         headers: {
+//             "Content-Type" : 'application/json',
+//         },
+//         body: JSON.stringify({
+//             name,
+//             shortDescription,
+//             description,
+//             image
+//         })
+//     })
+//     const newResponse = await response.json();
+//     console.log(newResponse)
+// });
+
+})();
+
+
