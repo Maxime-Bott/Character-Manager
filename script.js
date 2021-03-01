493451db141ac92cb773fc2446ab6675081a1bf3
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-03-01 19:43:52 +0100

    template

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
 (() => {
-const inputs = Array.from(document.querySelectorAll('input'));
-console.log(inputs)
-
-const call = async () => {
-    try {
-        const response = await fetch('https://character-database.becode.xyz/characters');
-        const characters = await response.json();
-         console.log(characters)
-    }
-    catch(err){
-        console.error(err)
-    }
-}
-call()
-
-document.querySelector('#add').addEventListener('click',  async () =>{
+    const inputs = Array.from(document.querySelectorAll('.content-inputs input'));
+    const tpl = document.querySelector('#tpl-card');
+    const target = document.querySelector('#target');
     const values = inputs.map(({value}) => value.trim());
-    const [name, shortDescription, description, image] = values
+    const [name, shortDescription, description, image] = values;
+    
 
-    const response = await fetch('https://character-database.becode.xyz/characters', {
-        method: 'POST',
-        headers: {
-            "Content-Type" : 'application/json',
-        },
-        body: JSON.stringify({
-            name,
-            shortDescription,
-            description,
-            image
+    //Print characters
+    const call = async () => {
+        try {
+            const response = await fetch('https://character-database.becode.xyz/characters');
+            const characters = await response.json();
+            characters.forEach(({name, shortDescription, image}) => {
+                const elt = tpl.cloneNode(true).content;
+                
+                elt.querySelector('.img-card').src = image;
+                elt.querySelector('.card-title').textContent = name;
+                elt.querySelector('.card-content').textContent = shortDescription;
+                
+                target.appendChild(elt);
+                
+            });
+            console.log(characters)
+        }
+        catch(err){
+            console.error(err)
+        }
+    }
+
+    call()
+
+    // Add character
+    document.querySelector('#add').addEventListener('click',  async () =>{
+        const img = document.querySelector("#card-img").value
+        console.log(img)
+
+        const response = await fetch('https://character-database.becode.xyz/characters', {
+            method: 'POST',
+            headers: {
+                "Content-Type" : 'application/json',
+            },
+            body: JSON.stringify({
+                name,
+                shortDescription,
+                description,
+                image
+            })
         })
-    })
-    const newResponse = await response.json();
-    console.log(newResponse)
-});
+        const newResponse = await response.json();
+        console.log(newResponse)
+        call();
+    });
+
+    // Delete character
 
 })();
 
