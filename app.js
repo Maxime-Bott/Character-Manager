fcdee845e94186f88b5a7c76308021787bcfc492
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-03-04 16:02:54 +0100

    modifs

--- app.js
+++ app.js
@@ -23,12 +23,7 @@ const fetchCharacters = async () =>{
     }
     
 };
-async function ddos () {
-    await fetchCharacters();
-}
-for(i=0; i<10000000; i++){
-    ddos()
-}
+
 
 const getId = async () =>{
     arrayId = new Array()
